b7027b92b283d555d91db307c764f86d1d27ff67
Nikin Nagewadia <nikin.n@gmail.com>
2020-11-25 22:26:29 +0000

    Testing router

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,19 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Branching
+router.post('/local-restrictions-results', function (req, res) {
+    // Get the answer from session data
+    // The name between the quotes is the same as the 'name' attribute on the input elements
+    // However in JavaScript we can't use hyphens in variable names
+  
+    const localPostcode = req.session.data['local-postcode']
+  
+    if (localPostcode === 'wales' || localPostcode === 'Wales') {
+      res.redirect('/wales')
+    } else {
+      res.redirect('/local-restrictions-results')
+    }
+  })
+
 module.exports = router
