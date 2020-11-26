3ce36df07b6c12df84c835405a4d318ce3c91e92
Nikin Nagewadia <nikin.n@gmail.com>
2020-11-26 21:00:56 +0000

    Travel checker routing business

--- app/routes.js
+++ app/routes.js
@@ -7,19 +7,18 @@ const readingPostcode = 'rg1 2lu';
 const bristolPostcode = 'bs1 5tr';
 const walesPostcode = 'cf11 0ba';
 
-// Results page
-const tier1 = '/tier1';
-const tier2 = '/tier2';
-const tier3 = '/tier3';
+// Reusable templates
 const devolved = '/devolved';
 const error = '/error';
-
 
 // Local restrictions
 router.post('/local-restrictions-results', function (req, res) {
   const localPostcode = req.session.data['local-postcode'];
 
   const localResults = '/local-restrictions-results';
+  const tier1 = '/tier1';
+  const tier2 = '/tier2';
+  const tier3 = '/tier3';
 
   const cornwallLocal = postCodeFormater(localPostcode, cornwallPostcode);
   const readingLocal = postCodeFormater(localPostcode, readingPostcode);
@@ -36,28 +35,44 @@ router.post('/local-restrictions-results', function (req, res) {
   }
 })
 
-
 // travel restrictions
 router.post('/travel-restrictions-results', function (req, res) {
   
   const originPostcode = req.session.data['origin-postcode'];
   const destinationPostcode = req.session.data['destination-postcode'];
 
-  const travelResults = '/travel-results';
+  const travelResults = '/travel-restrictions-results';
+  const tier2ToTier3 = '/tier2-tier3';
+  const tier2ToTier1 = '/tier2-tier1';
+  const tier2ToDevolved = '/tier2-devolved';
+  const tier3ToTier2 = '/tier3-tier2';
+  const tier1ToTier3 = '/tier1-tier3';
 
-  const home = postCodeFormater(originPostcode, homePostCode);
-  const friend = postCodeFormater(destinationPostcode, friendPostCode);
+  const cornwallStart = postCodeFormater(originPostcode, cornwallPostcode);
+  const readingStart = postCodeFormater(originPostcode, readingPostcode);
+  const bristolStart = postCodeFormater(originPostcode, bristolPostcode);
+  const walesStart = postCodeFormater(originPostcode, walesPostcode);
+  const cornwallEnd = postCodeFormater(destinationPostcode, cornwallPostcode);
+  const readingEnd = postCodeFormater(destinationPostcode, readingPostcode);
+  const bristolEnd = postCodeFormater(destinationPostcode, bristolPostcode);
+  const walesEnd = postCodeFormater(destinationPostcode, walesPostcode);
 
-  if (home && friend) {
-    res.redirect('/local-restrictions-results')
+  if (readingStart && bristolEnd) {
+    res.redirect(travelResults + tier2ToTier3)
+  } else if (readingStart && cornwallEnd) {
+    res.redirect(travelResults + tier2ToTier1)
+  } else if (readingStart && walesEnd) {
+    res.redirect(travelResults + tier2ToDevolved)
+  } else if (bristolStart && readingEnd) {
+    res.redirect(travelResults + tier3ToTier2)
+  } else if (cornwallStart && bristolEnd) {
+    res.redirect(travelResults + tier1ToTier3)
   } else {
-    res.redirect('/wales')
+    res.redirect(travelResults + error)
   }
 })
 
-
 module.exports = router
-
 
 function postCodeFormater(inputField, postCode) {
   return inputField === postCode || inputField === postCode.toUpperCase() || inputField === postCode.replace(/\s/g,'') || inputField === postCode.toUpperCase().replace(/\s/g,'')
