0b1cee17330f8dc91072f9608e3513edb43de595
Nikin Nagewadia <nikin.n@gmail.com>
2020-11-26 19:27:13 +0000

    Local restrictions test

--- app/routes.js
+++ app/routes.js
@@ -1,36 +1,35 @@
 const express = require('express')
 const router = express.Router()
 
-// Postcode for devolved nations
-const walesPostCode = 'n12 8by';
-const scotlandPostCode = 'l5a 1t1';
-
 // Postcodes
-const homePostCode = 'l5h 1e5';
-const friendPostCode = 'm5h 2t2';
+const cornwallPostcode = 'tr7 1af';
+const readingPostcode = 'rg1 2lu';
+const bristolPostcode = 'bs1 5tr';
+const walesPostcode = 'cf11 0ba';
 
 // Results page
-const walesResults = '/wales';
-const scotlandResults = '/scotland';
+const tier1 = '/tier1';
+const tier2 = '/tier2';
+const tier3 = '/tier3';
+const devolved = '/devolved';
 
 
 // Local restrictions
 router.post('/local-restrictions-results', function (req, res) {
-  // Get the answer from session data
-  // The name between the quotes is the same as the 'name' attribute on the input elements
-  // However in JavaScript we can't use hyphens in variable names
-
   const localPostcode = req.session.data['local-postcode'];
 
   const localResults = '/local-results';
 
-  const wales = postCodeFormater(localPostcode, walesPostCode);
-  const scotland = postCodeFormater(localPostcode, scotlandPostCode);
+  const cornwallLocal = postCodeFormater(localPostcode, cornwallPostcode);
+  const readingLocal = postCodeFormater(localPostcode, readingPostcode);
+  const bristolLocal = postCodeFormater(localPostcode, bristolPostcode);
 
-  if (wales) {
-    res.redirect(localResults + walesResults)
-  } else if (scotland) {
-    res.redirect(localResults + scotlandResults)
+  if (cornwallLocal) {
+    res.redirect(localResults + tier1)
+  } else if (readingLocal) {
+    res.redirect(localResults + tier2)
+  } else if (bristolLocal) {
+    res.redirect(localResults + tier3)
   } else {
     res.redirect('/local-restrictions-results')
   }
@@ -39,10 +38,7 @@ router.post('/local-restrictions-results', function (req, res) {
 
 // travel restrictions
 router.post('/travel-restrictions-results', function (req, res) {
-  // Get the answer from session data
-  // The name between the quotes is the same as the 'name' attribute on the input elements
-  // However in JavaScript we can't use hyphens in variable names
-
+  
   const originPostcode = req.session.data['origin-postcode'];
   const destinationPostcode = req.session.data['destination-postcode'];
 
