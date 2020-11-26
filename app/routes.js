30c93c234d94fcd99eccb76d4a21fd99554fa799
Nikin Nagewadia <nikin.n@gmail.com>
2020-11-26 14:39:13 +0000

    Testing both local and travel restrictions

--- app/routes.js
+++ app/routes.js
@@ -1,25 +1,51 @@
 const express = require('express')
 const router = express.Router()
 
-// Variables for devolved nations
+// Postcode for devolved nations
 const walesPostCode = 'n12 8by';
+
+// Postcodes
+const homePostCode = 'l5h 1e5';
+const friendPostCode = 'm5h 2t2';
+
 
 // Local restrictions
 router.post('/local-restrictions-results', function (req, res) {
-    // Get the answer from session data
-    // The name between the quotes is the same as the 'name' attribute on the input elements
-    // However in JavaScript we can't use hyphens in variable names
-  
-    const localPostcode = req.session.data['local-postcode'];
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
 
-    const wales = postCodeFormater(localPostcode, walesPostCode);
+  const localPostcode = req.session.data['local-postcode'];
 
-    if (wales) {
-      res.redirect('/wales')
-    } else {
-      res.redirect('/local-restrictions-results')
-    }
-  })
+  const wales = postCodeFormater(localPostcode, walesPostCode);
+
+  if (wales) {
+    res.redirect('/wales')
+  } else {
+    res.redirect('/local-restrictions-results')
+  }
+})
+
+
+// travel restrictions
+router.post('/travel-restrictions-results', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  const originPostcode = req.session.data['origin-postcode'];
+  const destinationPostcode = req.session.data['destination-postcode'];
+
+  const origin = postCodeFormater(originPostcode, homePostCode);
+  const destination = postCodeFormater(destinationPostcode, friendPostCode);
+
+  if (origin && destination) {
+    res.redirect('/local-restrictions-results')
+  } else {
+    res.redirect('/wales')
+  }
+})
+
 
 module.exports = router
 
